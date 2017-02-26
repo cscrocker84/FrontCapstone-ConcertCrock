06b77cede0230dc409ad5bbf09d7657b5e1c003f
Charlie Crocker <cscrocker84@gmail.com>
2017-02-26 13:02:12 -0600

    soft demo version

--- app/app.js
+++ app/app.js
@@ -1,9 +1,1 @@
 const app = angular.module('concertApp', ['ngRoute', 'angular-toArrayFilter']);
-
-    firebase.initializeApp({
-    apiKey: "AIzaSyDtdHAH3kQCS2jdHV96Gl7-Gsbz5FaXwu8",
-    authDomain: "capstonecharliesconcertcrock.firebaseapp.com",
-    databaseURL: "https://capstonecharliesconcertcrock.firebaseio.com",
-    storageBucket: "capstonecharliesconcertcrock.appspot.com",
-    messagingSenderId: "325860455476"
-});
